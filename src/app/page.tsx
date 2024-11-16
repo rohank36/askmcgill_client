3dfa24734111d725398bdfa75cde37610a3074fe
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2024-11-16 17:46:16 -0500

    Answer rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,29 +69,32 @@ export default function Home() {
             >Search
           </button>
         </form>
-      
-        <div className="flex flex-row w-full mt-10 my-10 px-10 gap-2">
-          <div className="px-10 py-2 border-2 rounded-lg mx-auto w-3/4 h-96 overflow-auto">
+
+        <div className="w-full">
+          {loading && (
             <div className="flex justify-center items-center">
               {loading && <span className="loading loading-spinner loading-lg text-red-500 mt-36"></span>}
             </div>
-            <div>
-              {!loading && answer && (
+          )}
+          {!loading && answer && (
+            <div className="flex flex-row w-full mt-10 my-10 px-10 gap-2">
+
+              <div className="px-10 py-2 border-2 rounded-lg mx-auto w-3/4 h-96 overflow-auto">
                 <div className="whitespace-pre-wrap">
                   <p>{answer}</p>
                 </div>
-              )}
+              </div>
+    
+              <div className="flex flex-col px-10 py-2 w-1/4 h-96 overflow-auto space-y-2">
+                  <h1>Answer Sources</h1>
+                  {sources.map((source, index) => (
+                    <SourceComponent key={index} source={source} />
+                  ))}
+              </div>
             </div>
-          </div>
-
-          <div className="flex flex-col px-10 py-2 w-1/4 h-96 overflow-auto space-y-2">
-              <h1>Answer Sources</h1>
-              {sources.map((source, index) => (
-                <SourceComponent key={index} source={source} />
-              ))}
-          </div>
+          )}
         </div>
-
+      
         <div>
             {!loading && answer && (
               <div>
