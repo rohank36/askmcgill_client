ccf18e82f108880784ffee9d7c600827c3b2c6e2
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2024-12-22 15:48:13 -0500

    Fixed modal issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,8 @@ export default function Home() {
               type="text" 
               id="query" 
               value={query} 
-              onChange={(e) => setQuery(e.target.value)}/>
+              onChange={(e) => setQuery(e.target.value)}
+              autoComplete="off"/>
  
             <button 
               type="submit" 
@@ -87,6 +88,39 @@ export default function Home() {
             </button>
           </form>
 
+          <div className="w-full">
+            {loading && (
+              <div className="flex justify-center items-center">
+                {loading && <span className="loading loading-spinner loading-lg text-red-500 mt-36"></span>}
+              </div>
+            )}
+            {!loading && answer && (
+              <div className="flex flex-row w-full mt-10 my-10 px-10 gap-2">
+
+                <div className="px-10 py-2 border-2 rounded-lg mx-auto w-3/4 h-96 overflow-auto">
+                  <div className="whitespace-pre-wrap">
+                    <p>{answer}</p>
+                  </div>
+                </div>
+      
+                <div className="flex flex-col px-10 py-2 w-1/4 h-96 overflow-auto space-y-2">
+                    <h1>Answer Sources</h1>
+                    {sources.map((source, index) => (
+                      <SourceComponent key={index} source={source} />
+                    ))}
+                </div>
+              </div>
+            )}
+          </div>
+      
+        <div>
+            {!loading && answer && (
+              <div>
+                <p className="font-bold text-slate-500">Answer generated in: {time.toFixed(4)} seconds</p>
+              </div>
+            )}
+        </div>
+
           <div className="flex absolute items-center justify-center bottom-3 gap-32">
             <h3 className="text-xs text-regularText">The information has been retrieved from McGill University's academic websites and may contain errors or need updates</h3>
           </div>
@@ -94,7 +128,7 @@ export default function Home() {
           <div className="toast toast-start gap-y-4">
             <button className="hover:scale-110 transition duration-500 ease-in-out" onClick={() => openModal(modalRef3)}><MessageCircle color={toastIconColor} size={20} /></button>
             <dialog ref={modalRef3} className="modal">
-              <div className="modal-boxt">
+              <div className="modal-box">
                 <h3 className="font-bold text-lg">Have any suggestions?</h3>
                 <div className="flex flex-col gap-y-4 py-4">
                   <p className="text-wrap">We'd love to hear your feedback to improve the product.</p>
@@ -143,37 +177,6 @@ export default function Home() {
 }
 
 /*
-<div className="w-full">
-          {loading && (
-            <div className="flex justify-center items-center">
-              {loading && <span className="loading loading-spinner loading-lg text-red-500 mt-36"></span>}
-            </div>
-          )}
-          {!loading && answer && (
-            <div className="flex flex-row w-full mt-10 my-10 px-10 gap-2">
 
-              <div className="px-10 py-2 border-2 rounded-lg mx-auto w-3/4 h-96 overflow-auto">
-                <div className="whitespace-pre-wrap">
-                  <p>{answer}</p>
-                </div>
-              </div>
-    
-              <div className="flex flex-col px-10 py-2 w-1/4 h-96 overflow-auto space-y-2">
-                  <h1>Answer Sources</h1>
-                  {sources.map((source, index) => (
-                    <SourceComponent key={index} source={source} />
-                  ))}
-              </div>
-            </div>
-          )}
-        </div>
-      
-        <div>
-            {!loading && answer && (
-              <div>
-                <p className="font-bold text-slate-500">Answer generated in: {time.toFixed(4)} seconds</p>
-              </div>
-            )}
-        </div>
         
 */
