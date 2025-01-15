141d2bac58dc2505707efa5d94b55dc2ef2d4882
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-01-15 13:56:05 -0500

    not affiliated with mcgill statement

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () =>{
     return(
         <div className="flex absolute items-center justify-center bottom-3 gap-32">
-            <h3 className="text-xs text-regularText">The information has been retrieved from McGill University's academic websites and may contain errors or need updates</h3>
+            <h3 className="text-xs text-regularText">The information has been retrieved from McGill University's academic websites and may contain errors or need updates. Not affiliated with McGill University.</h3>
         </div>
 
     );
